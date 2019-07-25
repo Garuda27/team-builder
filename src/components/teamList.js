8321c2f5285d1510c0c1e08f3df7fcc54d680f1e
Preston <pres.10.nap@gmail.com>
2019-07-25 02:30:31 -0400

    fell asleep working on it, here's what I have

---
+++ src/components/teamList.js
@@ -0,0 +1,16 @@
+const teamData = [
+    {
+        Name:"Preston",
+        email: "pres_10@gmail.com",
+        role: "Student",},
+    {
+        Name:"Brett",
+        email: "brett@gmail.com",
+        role: "TL",},
+    {
+        Name:"Dan",
+        email: "dan@gmail.com",
+        role: "Instructor",},
+]
+
+ export default teamData; 
